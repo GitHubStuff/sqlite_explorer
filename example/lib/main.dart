b07a28bbf1a526627f3670bd2eaae44a898086a5
Steven Smith <github@icodeforyou.com>
2021-06-19 05:48:16 -0400

    misspet

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_extras/flutter_extras.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:sqlite_explorer/sqlite_explorer.dart';
 import 'package:theme_manager/theme_manager.dart';
 import 'package:tracers_package/tracers.dart';
 
@@ -17,6 +18,15 @@ void main() async {
   /// Enable theme changes/monitoring by creating a 'hive' object to persist theme information
   await ThemeManager.setup();
   await Log.setTrace(baseLevel: LogLevel.All);
+
+  final cache = PersistedStringCache(cacheId: 'Fi');
+  for (int i = 0; i < 15; i++) {
+    cache.text = '$i';
+    final value = cache.text;
+    debugPrint('Input $value');
+  }
+  final List<String> items = cache.cachedItems();
+  debugPrint('$items');
 
   runApp(ModularApp(
     module: AppModule(),
--- lib/sqlite_explorer.dart
+++ lib/sqlite_explorer.dart
@@ -1,4 +1,5 @@
 library sqlite_explorer;
 
+export 'TextZ.dart';
 export 'moor/moor_bridge.dart';
 export 'src/sqlite_screen_widget.dart';
